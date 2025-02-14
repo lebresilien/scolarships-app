d71cbf1e9dcd86bc059849c1bb1bd8ae7f5a240d
lebresilien <lebresilienpepita@gmail.com>
2025-02-14 14:30:00 +0100

    ended trimester report

--- resources/views/pdf/trimester.blade.php
+++ resources/views/pdf/trimester.blade.php
@@ -107,6 +107,9 @@
             border: 0px;
             background-color: rgb(163 163 163);
         }
+        .bold {
+            font-weight: bold
+        }
     </style>
     <body>
 
@@ -419,7 +422,130 @@
 
             </div>
 
+            <div style="display: flex; column-gap: 3px; margin-top: 50px">
+
+                <div style="width: 50%">
+                    <table style="border-collapse: collapse;border: 1px solid black; width: 100%">
+                        <tbody>
+                            <tr>
+                                <td style="background-color: rgb(163 163 163); text-align: center" colspan="8">
+                                    Décisions du conseil de classe
+                                </td>
+                            </tr>
+                            <tr style="background-color: rgb(163 163 163)">
+                                <td class="text-align" colspan="3">DISCIPLINE</td>
+                                <td class="text-align" colspan="4">TRAVAIL</td>
+                            </tr>
+                            <tr>
+                                <td rowspan="2" style="border: 1px solid black">Absences</td>
+                                <td class="bold">J</td>
+                                <td class="text-align bold"><span style="display: block; padding: 10px"></span></td>
+                                <td class="text-align bold">TB</td>
+                                <td class="text-align bold">B</td>
+                                <td class="text-align bold">AB</td>
+                                <td class="text-align bold">PASS</td>
+                            </tr>
+                            <tr>
+                                <td style="border: 1px solid black; font-weight: bold">NJ</td>
+                                <td class="text-align bold"><span style="display: block; padding: 10px"></span></td>
+                                <td class="text-align bold">MED</td>
+                                <td class="text-align bold">INS</td>
+                                <td class="text-align bold">FAIB</td>
+                                <td class="text-align bold">NUL</td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">Avertissement</td>
+                                <td></td>
+                                <td rowspan="3" colspan="4">
+                                    <div style="display: flex: flex-direction: column; width: 100%; height: 100%; line-height: 0.1">
+                                        <p style="text-align: end; font-size: 15px;  margin-right: 10px">Un effort s'impose en</p>
+                                        <p style="border-bottom: 1px solid black; margin-left: 10px; margin-right: 10px"></p>
+                                        <p style="border-bottom: 1px solid black; margin-left: 10px; margin-right: 10px"></p>
+                                        <p style="border-bottom: 1px solid black; margin-left: 10px; margin-right: 10px"></p>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">Blâmes de conduite</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">Exclusions (en jours)</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td style="text-align: center; background-color: rgb(163 163 163)" colspan="3">Visa des parents ou tuteurs</td>
+                                <td style="text-align: center; background-color: rgb(163 163 163)" colspan="4">Observation et visa du professeur titulaire</td>
+                            </tr>
+                            <tr>
+                                <td style="height: 70px" colspan="3"></td>
+                                <td style="height: 70px" colspan="4"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div style="width: 50%; border: 1px solid black">
+                   
+                   @php
+                        $average_tri = number_format($semester_total_pound/$semester_total_coefficient, 2);
+                    @endphp
+
+                    <div style="display: flex; background-color: rgb(163 163 163)">
+                        <div style="display: flex; flex-direction: column; width: 60%; border-right: 1px solid black; font-size: 14px; row-gap: 5px">
+                            <span>Elève/Classe</span>
+                            <div style="display: flex; justify-content: space-around">
+                                <span>Rang</span>
+                                <span>{{ $range }}/{{ $record['current_classroom']['count_student'] }}</span>
+                                <span>Max</span>
+                                <span>{{ round($statistics->max('average'), 2) }}</span>
+                            </div>
+                            <div style="display: flex; justify-content: space-around">
+                                <span>Moy</span>
+                                <span>{{ $average_tri }}</span>
+                                <span>Min</span>
+                                <span>{{ round($statistics->min('average'), 2) }}</span>
+                            </div>
+                        </div>
+                        <div style="display: flex; flex-direction: column; width: 40%">
+                            <span style="font-size: 15px">MENTION</span>
+                            {{-- <div style="display: flex; justify-content: center; text-transform: uppercase"></div> --}}
+                            <div style="display: flex; border-top: 1px solid black; text-transform: uppercase; align-items: center; justify-content: center; height: 100%">
+
+                                @if($average_tri < 10)
+                                    <span class="text-align">Compétences non acquises</span>
+                                @elseif ($average_tri >= 10 && $average_tri < 12)
+                                    <span class="text-align">Compétences moyennement acquises</span>
+                                @elseif ($average_tri >= 12 && $average_tri < 15)
+                                    <span class="text-align">Compétences acquises</span>
+                                @else
+                                    <span class="text-align">Compétences bien acquises</span>
+                                @endif
+
+                            </div>
+                        </div>
+                    </div>
+                    <div style="margin-top: 57px">
+                        <div style="display: flex; justify-content: center;">Visa du principal</div>
+                        <div style="border-top: 1px solid black; height: 100px"></div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
+
+        <button onclick="imprimer('container')" style="margin-top: 10px; padding: 10px">Imprimer</button>
+
+        <script>
+            function imprimer(container){
+                var printContents = document.getElementById(container).innerHTML;
+                var originalContents = document.body.innerHTML;
+                document.body.innerHTML = printContents;
+                window.print();
+                document.body.innerHTML = originalContents;
+            }
+        </script>
 
     </body>
 </html>
